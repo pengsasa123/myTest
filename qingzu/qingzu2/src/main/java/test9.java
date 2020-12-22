45c2bbc4a03c9ae4560384aa75ea604e91e20318
彭萨萨 <pengsasa@qingzu.com.cn>
2020-12-22 16:23:52 +0800

    dfafa

--- qingzu/qingzu2/src/main/java/test9.java
+++ qingzu/qingzu2/src/main/java/test9.java
@@ -9,7 +9,17 @@ public class test9 {
             .forEach(System.out::println);*/
 
         // 合并两个stream
-        Stream.concat(Stream.of(1, 2, 3, 6, 7), Stream.of(1, 2, 3, 4, 5))
-            .forEach(System.out::println);
+       /* Stream.concat(Stream.of(1, 2, 3, 6, 7), Stream.of(1, 2, 3, 4, 5))
+            .forEach(System.out::println);*/
+
+        // 等同于下面
+        Stream.iterate(0, d -> ++d)
+            .limit(20).forEach(System.out::println);
+
+        System.out.println("----------------------------------");
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(i);
+        }
     }
 }
