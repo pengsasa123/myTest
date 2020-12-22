b0f7227a9dbac68e2921bdd7bcb73bc56c2947ab
彭萨萨 <pengsasa@qingzu.com.cn>
2020-12-22 16:30:27 +0800

    fatet

--- qingzu/qingzu2/src/main/java/test9.java
+++ qingzu/qingzu2/src/main/java/test9.java
@@ -2,17 +2,16 @@ import java.util.stream.Stream;
 
 public class test9 {
     public static void main(String[] args) {
-        // 无限流  从2开始每次加2
-        // Stream.iterate(2, x -> x + 2);
         // 无限流 无参,有返回值
-        /*Stream.generate(() -> Math.random())
-            .forEach(System.out::println);*/
+        Stream.generate(() -> Math.random())
+            .limit(10)
+            .forEach(System.out::println);
 
         // 合并两个stream
        /* Stream.concat(Stream.of(1, 2, 3, 6, 7), Stream.of(1, 2, 3, 4, 5))
             .forEach(System.out::println);*/
 
-        // 等同于下面
+        // 无限流 等同于下面 取20条数据
         Stream.iterate(0, d -> ++d)
             .limit(20).forEach(System.out::println);
 
@@ -21,5 +20,7 @@ public class test9 {
         for (int i = 0; i < 20; i++) {
             System.out.println(i);
         }
+
+
     }
 }
