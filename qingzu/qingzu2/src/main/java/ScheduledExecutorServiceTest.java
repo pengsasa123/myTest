7a8b11577f51bb4171077ee8de6d3047c29aad3e
彭萨萨 <pengsasa@qingzu.com.cn>
2021-02-22 15:07:41 +0800

    fadfa

--- qingzu/qingzu2/src/main/java/ScheduledExecutorServiceTest.java
+++ qingzu/qingzu2/src/main/java/ScheduledExecutorServiceTest.java
@@ -8,14 +8,14 @@ public class ScheduledExecutorServiceTest {
     public static ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(4, new ThreadPoolExecutor.DiscardPolicy());
 
     public static void main(String[] args) {
-        //等待任务执行完毕再进行下一步
+        //等待任务执行完毕再进行下一步(以上一次任务的结束时间计算下一次任务的开始时间)
         scheduledExecutorService.scheduleWithFixedDelay(() -> {
 
             System.out.println("1234567890");
         }, 3, 1, TimeUnit.SECONDS);
 
 
-        //不管任务是否执行完毕,到时间就执行
+        //不管任务是否执行完毕,到时间就执行(按照上一次任务的发起时间计算下一次任务的开始时间)
         scheduledExecutorService.scheduleAtFixedRate(() -> {
 
             System.out.println("1234567890");
